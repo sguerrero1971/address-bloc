7a6b3635b9878c28628c87692f2a6cf6711e2858
Sergio Guerrero <sguerrero1971@gmail.com>
2016-03-03 14:37:10 -0500

    Checkpoint 22 Searching

--- models/address_book.rb
+++ models/address_book.rb
@@ -28,6 +28,25 @@ class AddressBook
     end
   end
 
+    # Search AddressBook for a specific entry by name
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+    while lower <= upper
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+      if name == mid_name
+           return entries[mid]
+         elsif name < mid_name
+           upper = mid - 1
+         elsif name > mid_name
+           lower = mid + 1
+         end
+       end
+    return nil
+  end
+
+
   def remove_entry(name, phone_number, email)
     d_entry = Array.new
     @entries.each do |entry|

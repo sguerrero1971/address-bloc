052ce9c2510bbf8a159e019240fdf18ae921298a
Sergio Guerrero <sguerrero1971@gmail.com>
2016-03-03 13:26:54 -0500

    Assignment-20-menu

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
   def main_menu
     puts "Main Menu - #{address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View Entry Number n"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection: "
     selection = gets.to_i
     puts "You picked #{selection}"
@@ -24,17 +25,21 @@ class MenuController
         main_menu
       when 2
         system "clear"
-        create_entry
+        view_entry_by_number
         main_menu
       when 3
         system "clear"
-        search_entries
+        create_entry
         main_menu
       when 4
         system "clear"
-        read_csv
+        search_entries
         main_menu
       when 5
+        system "clear"
+        read_csv
+        main_menu
+      when 6
         puts "Good-bye!"
         exit(0)
       else
@@ -52,6 +57,21 @@ class MenuController
 
       system "clear"
       puts "End of entries"
+  end
+
+  def view_entry_by_number
+    puts "Input Entry Number"
+    print "Number: "
+      number = gets.chomp.to_i
+      item = address_book.entries.fetch(number, "oops!")
+      if item == "oops!"
+        system "clear"
+        puts "#{number} is not a valid input, try again"
+        view_entry_by_number
+      else
+        system "clear"
+        puts item
+      end
   end
 
   def create_entry
